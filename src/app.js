7f2e0ccdc22259346fe1839fc137a12fbf9dc93e
Salma Sayed <salmasayed797@gmail.com>
2019-11-14 15:40:42 +0200

    init project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+const axios = require("axios").default;
+
+ReactDom.render(<p>init project</p>, document.getElementById("app"));
