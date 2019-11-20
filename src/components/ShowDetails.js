b98efdb0e51822ad4353c9005235280184f9673d
Salma Sayed <salmasayed797@gmail.com>
2019-11-20 14:41:04 +0200

    trying to fix icon issue

--- src/components/ShowDetails.js
+++ src/components/ShowDetails.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { getShow } from "../utils/APIs/axios";
+import { Grid, Typography } from "@material-ui/core";
+import ShowCard from "./ShowCard";
+
 class ShowDetails extends React.Component {
+  state = {
+    show: undefined
+  };
   componentDidMount() {
     getShow(this.props.match.params.id).then(response => {
       console.log(response.data);
+      this.setState({
+        show: response.data
+      });
     });
   }
   render() {
-    return <div>ShowDetails</div>;
+    console.log(this.state.show);
+    return (
+      <Grid container={true}>
+        <Grid item={true}>
+          <ShowCard show={this.state.show} />
+        </Grid>
+        <Grid item={true}>
+          <Typography variant="heading">{this.state.show.name}</Typography>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ShowsSearch from "../components/ShowsSearch";
 import ShowDetails from "../components/ShowDetails";
 import NotFoundPage from "../components/NotFoundPage";
@@ -9,8 +9,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={ShowsSearch} exact={true} />
         <Route path="/show/:id" component={ShowDetails} />
-        <Route path="/notfound" component={NotFoundPage} />
-        <Redirect to="/notfound" />
+        <Route component={NotFoundPage} />
       </Switch>
     </div>
   </BrowserRouter>
