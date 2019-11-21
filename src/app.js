1ba1d7aa1eaa522405e7c4dfb73d883eca0bf3e4
Salma Sayed <salmasayed797@gmail.com>
2019-11-21 11:45:59 +0200

    Updates

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "normalize.css/normalize.css";
-import "./styles/styles.scss";
-import AppRouter from "./routers/AppRouter";
-
-ReactDom.render(<AppRouter />, document.getElementById("app"));
